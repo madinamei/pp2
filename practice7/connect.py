5e6b546650b35d1666071ab92be8eee371bf9676
Madina <madinamei200812@gmail.com>
2026-04-10 21:39:24 +0500

    f

--- practice7/connect.py
+++ practice7/connect.py
@@ -1,17 +1,9 @@
 import psycopg2
-from config import load_config
 
-def connect(config):
-    """ Connect to the PostgreSQL database server """
-    try:
-        # connecting to the PostgreSQL server
-        with psycopg2.connect(**config) as conn:
-            print('Connected to the PostgreSQL server.')
-            return conn
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
-
-
-if __name__ == '__main__':
-    config = load_config()
-    connect(config)
+def connect():
+    return psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="madinameiramova",
+        password="postgres"
+    )
--- practice7/phonebook.py
+++ practice7/phonebook.py
@@ -1,176 +1,90 @@
-import psycopg2
-from config import load_config
+from connect import connect
+import csv
 
+# CREATE (добавление)
+def insert_contact(name, phone):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO phonebook (name, phone) VALUES (%s, %s)", (name, phone))
+    conn.commit()
+    cur.close()
+    conn.close()
 
-# 🔹 CREATE TABLES
-import psycopg2
-from config import load_config
-def create_tables():
-    """ Create tables in the PostgreSQL database"""
-    commands = (
-        """
-        CREATE TABLE vendors (
-            vendor_id SERIAL PRIMARY KEY,
-            vendor_name VARCHAR(255) NOT NULL
-        )
-        """,
-        """ CREATE TABLE parts (
-                part_id SERIAL PRIMARY KEY,
-                part_name VARCHAR(255) NOT NULL
-                )
-        """,
-        """
-        CREATE TABLE part_drawings (
-                part_id INTEGER PRIMARY KEY,
-                file_extension VARCHAR(5) NOT NULL,
-                drawing_data BYTEA NOT NULL,
-                FOREIGN KEY (part_id)
-                REFERENCES parts (part_id)
-                ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE vendor_parts (
-                vendor_id INTEGER NOT NULL,
-                part_id INTEGER NOT NULL,
-                PRIMARY KEY (vendor_id , part_id),
-                FOREIGN KEY (vendor_id)
-                    REFERENCES vendors (vendor_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (part_id)
-                    REFERENCES parts (part_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """)
-    try:
-        config = load_config()
-        with psycopg2.connect(**config) as conn:
-            with conn.cursor() as cur:
-                # execute the CREATE TABLE statement
-                for command in commands:
-                    cur.execute(command)
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
-if __name__ == '__main__':
-    create_tables()
+# READ (просмотр)
+def get_contacts():
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM phonebook")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
 
-# 🔹 INSERT ONE
-import psycopg2
-from config import load_config
-def insert_vendor(vendor_name):
-    """ Insert a new vendor into the vendors table """
-    sql = """INSERT INTO vendors(vendor_name)
-             VALUES(%s) RETURNING vendor_id;"""
-    vendor_id = None
-    config = load_config()
-    try:
-        with  psycopg2.connect(**config) as conn:
-            with  conn.cursor() as cur:
-                # execute the INSERT statement
-                cur.execute(sql, (vendor_name,))
-                # get the generated id back
-                rows = cur.fetchone()
-                if rows:
-                    vendor_id = rows[0]
-                # commit the changes to the database
-                conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        return vendor_id
-if __name__ == '__main__':
-    insert_vendor("3M Co.")
+# UPDATE
+def update_contact(name, new_phone):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("UPDATE phonebook SET phone=%s WHERE name=%s", (new_phone, name))
+    conn.commit()
+    cur.close()
+    conn.close()
 
+# DELETE
+def delete_contact(name):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM phonebook WHERE name=%s", (name,))
+    conn.commit()
+    cur.close()
+    conn.close()
 
-# 🔹 INSERT MANY
-def insert_many_vendors(vendor_list):
-    """ Insert multiple vendors into the vendors table  """
-    sql = "INSERT INTO vendors(vendor_name) VALUES(%s) RETURNING *"
-    config = load_config()
-    try:
-        with  psycopg2.connect(**config) as conn:
-            with  conn.cursor() as cur:
-                # execute the INSERT statement
-                cur.executemany(sql, vendor_list)
-            # commit the changes to the database
-            conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+# CSV импорт
+def import_csv(file):
+    conn = connect()
+    cur = conn.cursor()
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            cur.execute("INSERT INTO phonebook (name, phone) VALUES (%s, %s)", (row[0], row[1]))
+    conn.commit()
+    cur.close()
+    conn.close()
 
-if __name__ == '__main__':
-    insert_vendor("3M Co.")
+# Меню
+def menu():
+    while True:
+        print("\n1. Add contact")
+        print("2. Show contacts")
+        print("3. Update contact")
+        print("4. Delete contact")
+        print("5. Import CSV")
+        print("0. Exit")
 
-    insert_many_vendors([
-        ('AKM Semiconductor Inc.',),
-        ('Asahi Glass Co Ltd.',),
-        ('Daikin Industries Ltd.',),
-        ('Dynacast International Inc.',),
-        ('Foster Electric Co. Ltd.',),
-        ('Murata Manufacturing Co. Ltd.',)
-    ])
+        choice = input("Choose: ")
 
-# 🔹 UPDATE
-import psycopg2
-from config import load_config
-def update_vendor(vendor_id, vendor_name):
-    """ Update vendor name based on the vendor id """
-    updated_row_count = 0
-    sql = """ UPDATE vendors
-                SET vendor_name = %s
-                WHERE vendor_id = %s"""
-    config = load_config()
-    try:
-        with  psycopg2.connect(**config) as conn:
-            with  conn.cursor() as cur:
-                # execute the UPDATE statement
-                cur.execute(sql, (vendor_name, vendor_id))
-                updated_row_count = cur.rowcount
-            # commit the changes to the database
-            conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        return updated_row_count
-if __name__ == '__main__':
-    update_vendor(1, "3M Corp")
+        if choice == "1":
+            name = input("Name: ")
+            phone = input("Phone: ")
+            insert_contact(name, phone)
 
-# 🔹 DELETE
-import psycopg2
-from config import load_config
-def delete_part(part_id):
-    """ Delete part by part id """
-    rows_deleted  = 0
-    sql = 'DELETE FROM parts WHERE part_id = %s'
-    config = load_config()
-    try:
-        with  psycopg2.connect(**config) as conn:
-            with  conn.cursor() as cur:
-                # execute the UPDATE statement
-                cur.execute(sql, (part_id,))
-                rows_deleted = cur.rowcount
-            # commit the changes to the database
-            conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        return rows_deleted
-if __name__ == '__main__':
-    deleted_rows = delete_part(2)
-    print('The number of deleted rows: ', deleted_rows)
+        elif choice == "2":
+            get_contacts()
 
+        elif choice == "3":
+            name = input("Name: ")
+            phone = input("New phone: ")
+            update_contact(name, phone)
 
-# 🔹 MAIN
-if __name__ == '__main__':
-    create_tables()
+        elif choice == "4":
+            name = input("Name: ")
+            delete_contact(name)
 
-    insert_vendor("3M Co.")
+        elif choice == "5":
+            file = input("CSV file name: ")
+            import_csv(file)
 
-    insert_many_vendors([
-        ('AKM Semiconductor Inc.',),
-        ('Asahi Glass Co Ltd.',),
-        ('Daikin Industries Ltd.',)
-    ])
+        elif choice == "0":
+            break
 
-    update_vendor(1, "NEW NAME")
-
-    delete_part(1)
+menu()
